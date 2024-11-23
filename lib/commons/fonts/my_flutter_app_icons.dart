07f3548a7e17abd408502898fd7d6fda29c2b173
Mohamed Nidal <touhami9@gmail.com>
2024-11-24 00:23:39 +0100

    account screen completed

--- lib/commons/fonts/my_flutter_app_icons.dart
+++ lib/commons/fonts/my_flutter_app_icons.dart
@@ -21,6 +21,12 @@ class CustomFonts {
   static const _kFontFam = 'MyFlutterApp';
   static const String? _kFontPkg = null;
 
+  static const IconData calendar =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData doctor_outlined =
       IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pill =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dolar =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
--- lib/features/account/screen/account_screen.dart
+++ lib/features/account/screen/account_screen.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:pharmacy_app/commons/accordion.dart';
 import 'package:pharmacy_app/commons/fonts/my_flutter_app_icons.dart';
+import 'package:pharmacy_app/commons/text_title_large.dart';
 import 'package:pharmacy_app/features/account/components/account_profile_details.dart';
 import 'package:pharmacy_app/features/account/components/profile_saved_item.dart';
 
 class AccountScreen extends StatelessWidget {
-  const AccountScreen({Key? key}) : super(key: key);
+  const AccountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.all(16.0),
-      children: const [
-        AccountProfileDetails(),
-        SizedBox(
+      children: [
+        const AccountProfileDetails(),
+        const SizedBox(
           height: 30,
         ),
-        Row(
+        const Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             ProfileSavedItem(
@@ -33,23 +33,69 @@ class AccountScreen extends StatelessWidget {
             ),
           ],
         ),
-        Column(
-          children: [
-            Row(
-              children: [Icon(Icons.calendar_month_outlined)],
-            ),
-            Row(
-              children: [Icon(Icons.perm_identity_outlined)],
-            ),
-            Row(
-              children: [Icon(Icons.animation_outlined)],
-            ),
-            Row(
-              children: [Icon(CustomFonts.doctor_outlined)],
-            ),
-          ],
+        const SizedBox(
+          height: 30,
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: const Column(
+            children: [
+              AccountLinks(
+                iconData: CustomFonts.calendar,
+                title: 'Appointments',
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              AccountLinks(
+                iconData: CustomFonts.pill,
+                title: 'Pills Reminder',
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              AccountLinks(
+                iconData: CustomFonts.dolar,
+                title: 'My Doctors',
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              AccountLinks(
+                iconData: CustomFonts.doctor_outlined,
+                title: 'Insurance Plan',
+              ),
+            ],
+          ),
         ),
       ],
+    );
+  }
+}
+
+class AccountLinks extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+
+  const AccountLinks({super.key, required this.iconData, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            iconData,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          TextTileLarge(text: title),
+          const Icon(Icons.keyboard_arrow_right)
+        ],
+      ),
     );
   }
 }
